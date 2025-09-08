c61fe0e3298026b28b14eb3425b000861875502c
viken.gezekelian <viken.gezekelian@stonebranch.com>
2025-09-08 16:02:06 +0300

    feature(#13): Fix headers being passed onto the request

--- uac_api/task_instances.py
+++ uac_api/task_instances.py
@@ -1416,6 +1416,6 @@ class TaskInstances:
             url,
             json_data=payload,
             query=parameters,
-            headers={"Content-Type": "application/json", "Accept": "text/plain"},
+            headers={"Accept": "application/json", "Content-Type": "text/plain"},
             parse_response=False,
         )
