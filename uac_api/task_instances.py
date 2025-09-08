d4334a168a6fb8f3996384edb5de23a7d9e37fb1
viken.gezekelian <viken.gezekelian@stonebranch.com>
2025-09-08 17:22:28 +0300

    feature(#14): Fix naming of task_instance_id variable

--- uac_api/task_instances.py
+++ uac_api/task_instances.py
@@ -1420,13 +1420,15 @@ class TaskInstances:
             parse_response=False,
         )
 
-    def patch_task_instance(self, query=None, payload=None, taskinstanceID=None, **args):
+    def patch_task_instance(
+        self, query=None, payload=None, taskInstanceId=None, **args
+    ):
         """
         Arguments:
-        - taskinstanceID: taskinstanceID
+        - taskInstanceId: taskInstanceId
         - scope: scope  -- Only the 'fields' scope is supported as of UC 7.9
         """
-        url = f"/resources/taskinstance/{taskinstanceID}"
+        url = f"/resources/taskinstance/{taskInstanceId}"
         query_fields = {
             "scope": "scope",
         }
